1455d92168f893879f7a5a00eecb59c78db08600
SchalterDev <schalter.dev@gmail.com>
2017-07-15 12:51:09 +0200

    UI Elements
    Added custom Button, slider and toggle. Also added settings canvas

---
+++ Assets/Scripts/UI/customToggle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class customToggle : MonoBehaviour {
+
+	public Image background;
+	public GameObject toggleElement;
+
+	public bool checkedValue; 
+	public Color enabledColor;
+	public Color disabledColor;
+	public float fadeDuration; //in seconds
+
+	private Image toggleImage;
+	private Color color;
+	private float xPosition;
+
+	// Use this for initialization
+	void Start () {
+		toggleImage = toggleElement.GetComponent<Image> ();
+		setToggle (checkedValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		background.color = Color.Lerp (background.color, color, Time.deltaTime / fadeDuration);
+
+		float newXPosition = Mathf.Lerp (toggleElement.transform.localPosition.x, xPosition, Time.deltaTime / fadeDuration);
+		toggleElement.transform.localPosition = new Vector3 (newXPosition, toggleElement.transform.localPosition.y, toggleElement.transform.localPosition.z);
+	}
+
+	private void setToggle(bool enabled) {
+		if (enabled) {
+			xPosition = 58.4f;
+			color = enabledColor;
+		} else {
+			xPosition = -58.4f;
+			color = disabledColor;
+		}
+	}
+
+	public void toggle() {
+		checkedValue = !checkedValue;
+		setToggle (checkedValue);
+	}
+}
--- Assets/menu/menuManager.cs
+++ Assets/menu/menuManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class menuManager : MonoBehaviour {
 
+	public GameObject menuCanvas;
+	public GameObject settingsCanvas;
+
+	void Start() {
+		menuCanvas.SetActive (true);
+		settingsCanvas.SetActive (false);
+	}
+
 	public void startSingleplayer() {
 		Application.LoadLevel ("singleplayer");
 	}
 
 	public void openSettings() {
+		menuCanvas.SetActive (false);
+		settingsCanvas.SetActive (true);
+	}
 
+	public void openMenu() {
+		menuCanvas.SetActive (true);
+		settingsCanvas.SetActive (false);
 	}
 }
