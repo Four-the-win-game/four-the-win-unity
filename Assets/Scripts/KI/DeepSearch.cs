6312a428924d83dbdcdeccc06728e8db7e0cb441
SchalterDev <schalter.dev@gmail.com>
2017-07-25 00:22:51 +0200

    Ki beginning
    the really beginning of the ki

---
+++ Assets/Scripts/KI/DeepSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DeepSearch {
+	
+	public DeepSearch (GameBoard board, int firstInsertPos, int deep) {
+		List<int> turns = board.getValidTurns ();
+
+	}
+
+	
+
+}
+
---
+++ Assets/Scripts/KI/HardKI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class HardKI : Player {
+
+	private int rows;
+	private int columns;
+
+	private GameBoard board;
+
+	public HardKI () {
+
+	}
+
+	public int getNextMove (int player, GameBoard gameBoard) {
+		columns = gameBoard.boardColumns;
+		rows = gameBoard.boardRows;
+
+		board = gameBoard.clone ();
+
+
+		return 0;
+	}
+
+	public String getName() {
+		return "Hard AI";
+	}
+}
+
