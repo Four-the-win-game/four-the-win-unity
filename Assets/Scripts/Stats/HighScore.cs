1820e2a26f145cfc3e1d60f717fb44b65312ab1b
SchalterDev <schalter.dev@gmail.com>
2017-08-05 12:03:07 +0200

    Added player name in the settings

---
+++ Assets/Scripts/Stats/HighScore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class HighScore
+{
+	public static void winnedGameAgainstAi(int aiDifficulty, String name, int turns) {
+
+	}
+
+	public static void updateNameLastGame(String name) {
+
+	}
+}
+
---
+++ Assets/Scripts/UI/customInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class customInput : MonoBehaviour {
+
+	public string settingsTag;
+
+	public InputField inputField;
+
+	void Start() {
+		//Load value
+		Debug.Log("load: " + PlayerPrefs.GetString(settingsTag, LocalizationText.GetText ("player")));	 
+		inputField.text = PlayerPrefs.GetString(settingsTag, LocalizationText.GetText ("player"));
+	}
+
+	public void nameChanged(string name) {
+		Debug.Log ("Changed: " + inputField.text);
+		PlayerPrefs.SetString (settingsTag, inputField.text);
+	}
+}
